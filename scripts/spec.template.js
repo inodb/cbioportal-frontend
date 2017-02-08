a6fab7bb3f3d0a7ad63a4d951aa0c3cb59072828
Ino de Bruijn <ino@ino.pm>
2017-02-08 15:05:00 -0500

    add genereate spec file script

---
+++ scripts/spec.template.js
@@ -0,0 +1,21 @@
+import ## file here ##
+import React from 'react';
+import { assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+
+describe('## name of import here ##', () => {
+
+    before(()=>{
+
+    });
+
+    after(()=>{
+
+    });
+
+    it('what does it do?', ()=>{
+
+    });
+
+});
